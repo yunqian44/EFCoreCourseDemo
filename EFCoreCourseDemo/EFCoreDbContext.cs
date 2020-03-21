ffce02da56e1575c7f47153c81568741cb25351f
yunnqian44 <yunqian8@live.com>
2020-03-21 20:36:58 +0800

    data migrate

--- EFCoreCourseDemo/EFCoreDbContext.cs
+++ EFCoreCourseDemo/EFCoreDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Model;
+using System;
 
 namespace EFCoreCourseDemo
 {
@@ -16,13 +17,36 @@ namespace EFCoreCourseDemo
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseLoggerFactory(loggerFactory)
             .UseMySql(@"server=127.0.0.1;uid=root;pwd=qwer1234!;database=EFCore;Character Set = utf8;");
-        
+
 
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //针对于HasData限制（即使主键是由数据库生成的自动增长），也需要指定主键 
+
+            //调用EnsureCreated方法只针对与添加数据有效，但是数据库如果有数据的话，
+            //也就是对数据更改将无效
+            Console.WriteLine("**********Blog表开始初始化数据**********");
+            modelBuilder.Entity<Blog>().HasData(new Blog
+            {
+                Id=1,
+                Name="DDD领域驱动模型",
+            },new Blog
+            {
+                Id = 2,
+                Name = "EntityFramework Core",
+            });
+            modelBuilder.Entity<Post>().HasData(new Post
+            {
+                Id = 1,
+                BlogId=1,
+                Title="技术交流",
+                Content="第一次写博客，先随便写一下"
+            });
+
+           Console.WriteLine("**********Blog表开始初始化数据**********");
         }
     }
 }
--- EFCoreCourseDemo/Program.cs
+++ EFCoreCourseDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace EFCoreCourseDemo
 {
@@ -6,9 +7,20 @@ namespace EFCoreCourseDemo
     {
         static void Main(string[] args)
         {
+            #region 控制台程序迁移的方式  初始化使用的方式
             var context = new EFCoreDbContext();
             context.Database.EnsureDeleted();
+            Console.WriteLine("**********开始初始化数据**********");
             context.Database.EnsureCreated();
+            Console.WriteLine("**********初始化数据完成**********");
+            #endregion
+
+
+            #region 控制台程序迁移的方式  添加，修改使用的方式
+            Console.WriteLine("**********开始初始化数据**********");
+            context.Database.Migrate();
+            Console.WriteLine("**********初始化数据完成**********"); 
+            #endregion
 
             Console.ReadKey();
         }
