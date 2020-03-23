8b64d9589bb8f70bb5fc9e20c061b658bd7ba816
yunnqian44 <yunqian8@live.com>
2020-03-23 19:47:17 +0800

    Blog model add Field and Inherited  IUpdatedable interface

--- Model/Blog.cs
+++ Model/Blog.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 
 namespace Model
 {
-    public class Blog 
+    public class Blog :IUpdatedable
     {
+        /// <summary>
+        /// 主键Id
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreatedTime { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime UpdatedTime { get; set; }
+
         public List<Post> Posts { get; set; }
     }
 }
---
+++ Model/IUpdatedable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public interface IUpdatedable
+    {
+        DateTime UpdatedTime { get; set; }
+    }
+}
